cbe59f35a9b2e9b60551bf1e200da9e257d1673d
Carlos Biagolini-Jr. <c.biagolini@gmail.com>
2024-05-04 09:08:45 -0300

    Simplifying code for post medium

--- src/app/garage/garage.module.ts
+++ src/app/garage/garage.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
 
-import { GarageRoutingModule } from './garage-routing.module';
-import { GarageHomeComponent } from './components/garage-home/garage-home.component';
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from '../shared/shared.module'
+import { GarageHomeComponent } from './components/garage-home/garage-home.component'
+import { GarageRoutingModule } from './garage-routing.module'
 
 @NgModule({
   declarations: [GarageHomeComponent],
-  imports: [CommonModule, GarageRoutingModule, SharedModule],
+  imports: [CommonModule, GarageRoutingModule, SharedModule]
 })
 export class GarageModule {}
--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,29 +1,19 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+import { Component, ViewChild } from '@angular/core'
+import { MatSidenav } from '@angular/material/sidenav'
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss',
+  styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
-  @ViewChild('sidenav', { static: false }) sidenav!: MatSidenav;
+  @ViewChild('sidenav', { static: false }) sidenav!: MatSidenav
 
-  @ViewChild('sidenavEnd', { static: false }) sidenavEnd!: MatSidenav;
+  @ViewChild('sidenavEnd', { static: false }) sidenavEnd!: MatSidenav
 
-  showLeftSidenav: boolean = true;
+  showLeftSidenav: boolean = true
 
-  showRightSidenav: boolean = true;
+  showRightSidenav: boolean = true
 
-  mainArea: boolean = true;
-
-  changeLeft(): void {
-    console.log('changeLeft');
-    this.showLeftSidenav = !this.showLeftSidenav;
-  }
-
-  changeRight(): void {
-    console.log('changeRight');
-    this.showRightSidenav = !this.showRightSidenav;
-  }
+  mainArea: boolean = true
 }
