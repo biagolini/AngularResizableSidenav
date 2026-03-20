9bb1aab77ed33a6f61b2ce842d9f8c12162c6903
Marcello di Simone <m.disimone@synios.de>
2026-03-20 15:13:01 +0100

    update angular & material to 21

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import '@angular/localize/init'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
@@ -11,5 +12,5 @@ console.info('Angular CDK version', CDK_VERSION.full)
 console.info('Angular Material version', MAT_VERSION.full)
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+  .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err))
